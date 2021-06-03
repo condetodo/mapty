9ce1312ae9f462199a9c124e102fe090490ed3f4
condetodo <44619423+condetodo@users.noreply.github.com>
2021-06-03 18:24:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,6 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
-
 class Workout {
   date = new Date();
   id = Date.now() + ''.slice(-10);
@@ -54,6 +46,15 @@ class Cycling extends Workout {
 // ****************************************************
 
 // *************  APLICATION ARCHITECTURE *************
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
 class App {
   #map;
   #mapEvent;
@@ -105,14 +106,43 @@ class App {
   }
 
   _newWorkout(e) {
+    //Helper function
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+
+    const allPositive = (...inputs) => inputs.every(inp => inp);
+
     e.preventDefault();
     //Get data from the form
-
-    //Check if the data is valid
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
 
     //If Workout is runnning create object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+
+      //Check if the data is valid
+      if (
+        // !Number.isFinite(distance) ||
+        // !Number.isFinite(duration) ||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Inputs have to be positive numbers!');
+    }
 
     //If Workout is cycling create object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      //Check if the data is valid
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs have to be positive numbers!');
+    }
 
     // Add new object to the workout Array
 
